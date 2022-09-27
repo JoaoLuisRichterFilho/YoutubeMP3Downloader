71ac81759f89dfcdc76f28c2728eb39205e25ce5
JoaoLuisRichterFilho <jooluisrichter@gmail.com>
2022-09-26 22:50:33 -0300

    v2

--- Downloader.py
+++ Downloader.py
@@ -1,8 +1,20 @@
+from tkinter import filedialog
 from turtle import color, width
 from typing_extensions import Self
 from pytube import YouTube
 import os
 from tkinter import *
+import easygui
+
+
+
+def file_save(out_file):
+    file_path = filedialog.asksaveasfilename(confirmoverwrite=False, filetypes=[("MP3 File", ".mp3")], defaultextension=".mp3")
+    if(file_path):
+        os.rename(out_file, 'file.mp3')
+        os.replace('file.mp3', file_path)
+        easygui.msgbox("Sucesso!", title="Success Music Downloaded!")
+        
 
 def download():
     watch = str(inputWatch.get())
@@ -11,7 +23,7 @@ def download():
     # YouTube('https://youtu.be/'+watch).streams.first().download()
     yt = YouTube('https://youtu.be/'+watch)
     out_file = yt.streams.filter( mime_type="audio/mp4", abr="128kbps",  progressive=False, type="audio").first().download()
-    os.rename(out_file, 'new.mp3')
+    file_save(out_file)
 
 janela = Tk()
 janela.title("Download Youtube Music")
